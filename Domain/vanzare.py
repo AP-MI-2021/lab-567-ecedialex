9716af2545e389a36b1a8dad11068a24d84a35e9
Ecedi Alexandru <ecedialexandru@yahoo.com>
2021-10-27 13:50:44 +0300

    Domain cu dictionar

--- Domain/vanzare.py
+++ Domain/vanzare.py
@@ -8,14 +8,14 @@ def creeaza_vanzare(id_vanzare,titlu,gen,pret,tip_client):
     :param tip_client: tipul clientului ( none/silver/gold ) , nenul
     :return: o vanzare
     """
-    return [
-        id_vanzare,
-        titlu,
-        gen,
-        pret,
-        tip_client,
+    return {
+        'id': id_vanzare,
+        'titlu': titlu,
+        'gen' : gen,
+        'pret' : pret,
+        'reducere' : tip_client,
 
-    ]
+    }
 
 def get_id(vanzare):
     """
@@ -23,7 +23,7 @@ def get_id(vanzare):
     :param vanzare: vanzare
     :return: id-ul vanzarii date ca parametru
     """
-    return vanzare[0]
+    return vanzare['id']
 
 def get_titlu(vanzare):
     """
@@ -31,7 +31,7 @@ def get_titlu(vanzare):
     :param vanzare:  vanzare
     :return: titlul vanzarii date ca parametru
     """
-    return vanzare[1]
+    return vanzare['titlu']
 
 def get_gen(vanzare):
     """
@@ -39,7 +39,7 @@ def get_gen(vanzare):
     :param vanzare:  vanzare
     :return: genul vanzarii date ca parametru
     """
-    return vanzare[2]
+    return vanzare['gen']
 
 def get_pret(vanzare):
     """
@@ -47,7 +47,7 @@ def get_pret(vanzare):
     :param vanzare:  vanzare
     :return: pretul vanzarii date ca parametru
     """
-    return vanzare[3]
+    return vanzare['pret']
 
 def get_reducere(vanzare):
     """
@@ -55,7 +55,7 @@ def get_reducere(vanzare):
     :param vanzare:  vanzare
     :return: tipul reducerii vanzarii date ca parametru
     """
-    return vanzare[4]
+    return vanzare['reducere']
 
 def get_str(vanzare):
     return f'Vanzarea cu id-ul {get_id(vanzare)} a cartii {get_titlu(vanzare)} de genul {get_gen(vanzare)} a fost realizata cu pretul de {get_pret(vanzare)} catre un client {get_reducere(vanzare)}'
